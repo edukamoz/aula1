f98b390cd4126501b1b44cb4683be7c130464b29
Eduardo Kamo <gogeta1610@gmail.com>
2024-08-29 22:07:15 -0300

    Adicionando componentes

--- prjdsweb2/src/app/app.component.ts
+++ prjdsweb2/src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prjdsweb2';
+  title = 'Projeto DS Web II';
+  message = 'Hello World!'
 }
